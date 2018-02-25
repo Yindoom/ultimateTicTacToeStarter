0ade2d75ffb534aef62a7a43463c13b89e613f2d
mjl <mjl@easv.dk>
2018-02-25 22:19:48 +0100

    Updated the GameManager stub
    Added an intention description of the GameManager.
    Moved the vsBot code into the UpdageGame without parameters
    Changed the vsBot Boolean to a GameMode enum variable called mode.
    Moved the files out of src, to align with the original package naming.

--- game/GameManager.java
+++ game/GameManager.java
@@ -4,14 +4,29 @@ import bot.IBot;
 import move.IMove;
 
 /**
- *
+ * This is a proposed GameManager for Ultimate Tic-Tac-Toe,
+ * the implementation of which is up to whoever uses this interface.
+ * Note that initializing a game through the constructors means
+ * that you have to create a new instance of the game manager 
+ * for every new game of a different type (e.g. Human vs Human, Human vs Bot or Bot vs Bot),
+ * which may not be ideal for your solution, so you could consider refactoring
+ * that into an (re-)initialize method instead.
  * @author mjl
  */
 public class GameManager {
     
+    /**
+     * Three different game modes.
+     */
+    public enum GameMode{
+        HumanVsHuman,
+        HumanVsBot,
+        BotVsBot
+    }
+    
     private final IGameState currentState;
     private int currentPlayer = 0; //player0 == 0 && player1 == 1
-    private Boolean vsBot = false; //player1 will be a IBot
+    private GameMode mode = GameMode.HumanVsHuman;
     private IBot bot = null;
     private IBot bot2 = null;
 
@@ -23,6 +38,7 @@ public class GameManager {
      */
     public GameManager(IGameState currentState) {
         this.currentState = currentState;
+        mode = GameMode.HumanVsHuman;
     }
     
     /**
@@ -34,7 +50,7 @@ public class GameManager {
      */
     public GameManager(IGameState currentState, IBot bot) {
         this.currentState = currentState;
-        this.vsBot = true;
+        mode = GameMode.HumanVsBot;
         this.bot = bot;
     }
     
@@ -48,6 +64,7 @@ public class GameManager {
      */
     public GameManager(IGameState currentState, IBot bot, IBot bot2) {
         this.currentState = currentState;
+        mode = GameMode.BotVsBot;
         this.bot = bot;
         this.bot2 = bot2;
     }
@@ -71,28 +88,31 @@ public class GameManager {
         
         //Update currentPlayer
         currentPlayer = (currentPlayer + 1) % 2;
-            
+        
+        return true;
+    }
+    
+    /**
+     * Non-User driven input, e.g. an update for playing a bot move.
+     * @return Returns true if the update was successful, false otherwise.
+     */
+    public Boolean UpdateGame()
+    {
+        //Check game mode is set to one of the bot modes.
+        assert(mode != GameMode.HumanVsHuman);
+        
         //Check if player is bot, if so, get bot input and update the state based on that.
-        if(currentPlayer == 1 && vsBot)
+        if(mode == GameMode.HumanVsBot && currentPlayer == 1)
         {
             //Check bot is not equal to null, and throw an exception if it is.
             assert(bot != null);
             
             IMove botMove = bot.doMove(currentState);
             
-            //Be aware that your bots might perform illegal moves,
-            //so you may get a false, even if the user move was okay.
+            //Be aware that your bots might perform illegal moves.
             return UpdateGame(botMove);
         }
         
-        return true;
-    }
-    
-    /**
-     * Non-User driven input, e.g. an update for playing bots against each other.
-     */
-    public void UpdateGame()
-    {
         //Check bot is not equal to null, and throw an exception if it is.
         assert(bot != null);
         assert(bot2 != null);
@@ -103,7 +123,10 @@ public class GameManager {
     
     private Boolean VerifyMoveLegality(IMove move)
     {
-        //Test if the move is legal        
+        //Test if the move is legal   
+        //NOTE: should also check whether the move is placed on an occupied spot.
+        System.out.println("Checking move validity against macroboard available field");
+        System.out.println("Not currently checking move validity actual board");
         return currentState.getField().isInActiveMicroboard(move.getX(), move.getY());
     }
     
