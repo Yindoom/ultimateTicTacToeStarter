7b36038722298f3dd72ab1f278412218fb1e120e
Yindoom <31922816+Yindoom@users.noreply.github.com>
2018-02-26 10:27:55 +0100

    added interfaces

---
+++ ultimateTicTacToe/src/interfaces/IGameState.java
@@ -0,0 +1,19 @@
+package interfaces;
+
+import interfaces.IField;
+
+/**
+ *
+ * @author mjl
+ */
+public interface IGameState {
+
+    IField getField();
+
+    int getMoveNumber();
+    void setMoveNumber(int moveNumber);
+
+    int getRoundNumber();
+    void setRoundNumber(int roundNumber);
+    
+}
