0e7793258427ec379925946367cad724773d2518
Yindoom <31922816+Yindoom@users.noreply.github.com>
2018-02-26 10:31:54 +0100

    bllManager and Model

--- ultimateTicTacToe/src/ultimatetictactoe/GUI/TicTacToeController.java
+++ ultimateTicTacToe/src/ultimatetictactoe/GUI/TicTacToeController.java
@@ -8,7 +8,6 @@ package ultimatetictactoe.GUI;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
