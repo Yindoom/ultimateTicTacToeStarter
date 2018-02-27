f4cff2327c5af786f059106326d82b8b23823800
fabiomoniz <fabio_moniz_1@hotmail.com>
2018-02-27 11:40:43 +0000

    pushing

--- ultimateTicTacToe/src/ultimatetictactoe/GUI/TicTacToeController.java
+++ ultimateTicTacToe/src/ultimatetictactoe/GUI/TicTacToeController.java
@@ -7,10 +7,17 @@ package ultimatetictactoe.GUI;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 /**
  *
@@ -18,34 +25,74 @@ import javafx.scene.control.Label;
  */
 public class TicTacToeController implements Initializable {
     
+    private Node parentNode; // variable to save its parent
+    
+    private int GridX;
+    
+    private int GridY;
+    
     private Label label;
     @FXML
-    private Button b00;
+    private GridPane MainGrid;
     @FXML
-    private Button b10;
+    private AnchorPane MainAnchor;
     @FXML
-    private Button b01;
+    private GridPane grid_0_0;
     @FXML
-    private Button b11;
+    private GridPane grid_1_0;
     @FXML
-    private Button b20;
+    private GridPane grid_2_0;
     @FXML
-    private Button b21;
+    private GridPane grid_0_1;
     @FXML
-    private Button b02;
+    private GridPane grid_1_1;
     @FXML
-    private Button b12;
+    private GridPane grid_2_1;
     @FXML
-    private Button b22;
+    private GridPane grid_0_2;
+    @FXML
+    private GridPane grid_1_2;
+    @FXML
+    private GridPane grid_2_2;
     
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        
+        
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    private void dragOverAction(Node n, int x, int y) {  // universal method for drag over
+        
+    }
+    
+    @FXML
+    private void dragOver(MouseEvent event) {
+        
+        Node n = (Node)event.getSource(); // selects the grid pane that the mouse is currently over
+        
+        if (n.getParent() != MainAnchor) { // if statement added to avoid adding the same node to the anchor pane
+            String id = n.getId();
+            String[] parts = id.split("_");
+            GridX = Integer.parseInt(parts[2]);
+            GridY = Integer.parseInt(parts[1]);
+            MainGrid.getChildren().remove(n);
+            MainAnchor.getChildren().add(n);
+            
+        }
+    }
+
+    @FXML
+    private void dragExit(MouseEvent event) {
+        Node n = (Node)event.getSource();
+        MainAnchor.getChildren().remove(n);
+        MainGrid.add(n, GridY, GridX);
+        
+    }
+
+
     
 }
